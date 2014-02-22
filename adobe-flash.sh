cbafcd6f71253bfbf6155924f8edb77f5832c340
Findus23 <l.winkler23@me.com>
2014-02-22 17:48:02 +0100

    Adobe-Falsh hinzugefügt

---
+++ adobe-flash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get update
+if dpkg -l flashplugin-nonfree|grep -q ii
+then
+	echo "Flash ist bereits installiert"
+	zenity --info --title "Info" --text "Flash ist bereits installiert"
+	exit
+fi
+if apt-get install flashplugin-nonfree -y
+then
+	zenity --info --title "Erfolg" --text "Flash wurde erfolgreich installiert"
+else
+	zenity --error --title "Fehler" --text "Flash wurde nicht erfolgreich installiert"
+fi
