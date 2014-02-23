213f732e5d88d39192f94f4a1baab305289eb90f
Findus23 <l.winkler23@me.com>
2014-02-23 18:38:14 +0100

    Chromium hinzugefügt

---
+++ chromium.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#apt-get update
+if dpkg -l chromium|grep -q ii
+then
+	echo "Chromium ist bereits installiert"
+	zenity --info --title "Info" --text "Chromium ist bereits installiert"
+	exit
+fi
+
+if sudo apt-get install chromium
+then
+	zenity --info --title "Erfolg" --text "Chromium wurde erfolgreich installiert"
+else
+	zenity --error --title "Fehler" --text "Chromium wurde nicht erfolgreich installiert"
+fis
