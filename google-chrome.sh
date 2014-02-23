d79ef04011a6195aff3f6da5222947c27749578e
Findus23 <l.winkler23@me.com>
2014-02-23 15:44:19 +0100

    Google Chrome hinzugefügt

---
+++ google-chrome.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#apt-get update
+if dpkg -l google-chrome|grep -q ii
+then
+	echo "Google Chrome ist bereits installiert"
+	zenity --info --title "Info" --text "Google Chrome ist bereits installiert"
+	exit
+fi
+wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+
+if dpkg -i /tmp/chrome.deb -y
+then
+	zenity --info --title "Erfolg" --text "Google Chrome wurde erfolgreich installiert"
+else
+	zenity --error --title "Fehler" --text "Google Chrome wurde nicht erfolgreich installiert"
+fi
+rm /tmp/chrome.deb
