df67324a75b19fe506000a64f419baaf0c9794b9
Findus23 <l.winkler23@me.com>
2014-02-24 18:55:13 +0100

    wget Fortschrittsanzeige deaktiviert

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ if [ ! -f "version.txt" ] || [ "$version_local" -ne "$version_tag" ]
 then
 	echo "$version_tag ist aktueller als $version_local"
 	git reset --hard $version_tag
-	wget -O releases.json https://api.github.com/repos/Findus23/la-tools/releases
+	wget -O releases.json -q https://api.github.com/repos/Findus23/la-tools/releases 
 	name=$(grep -Po -m 1 '"name":.*?[^\\]",' releases.json| cut -c 10- | rev | cut -c 3- | rev)
 	body=$(grep -Po -m 1 '"body":.*?[^\\]",' releases.json| cut -c 10- | rev | cut -c 3- | rev)
 	echo $version_tag > version.txt
