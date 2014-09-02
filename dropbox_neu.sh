43177a3beeb7f6db18bce0000006151cc0dfd92f
Findus23 <l.winkler23@me.com>
2014-09-02 16:25:26 +0200

    Dropbox via Reposity

---
+++ dropbox_neu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if dpkg -l dropbox|grep -q ii
+then
+	if apt-get install geogebra44 -y
+	then
+		zenity --info --title "Erfolg" --text "Dropbox wurde erfolgreich installiert"
+	else
+		zenity --error --title "Fehler" --text "Dropbox wurde nicht erfolgreich installiert"
+	fi
+	exit
+fi
+sources=$(cat /etc/apt/sources.list.d/dropbox.list)
+if [ "$sources" != "deb http://linux.dropbox.com/debian wheezy main" ]
+then
+	echo "deb http://linux.dropbox.com/debian wheezy main" > temp
+	mv temp /etc/apt/sources.list.d/geogebra.list
+	sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+	rm temp
+fi
+apt-get update
+if apt-get install dropbox -y
+then
+	zenity --info --title "Erfolg" --text "Dropbox wurde erfolgreich installiert"
+else
+	zenity --error --title "Fehler" --text "Dropbox wurde nicht erfolgreich installiert"
+fi
