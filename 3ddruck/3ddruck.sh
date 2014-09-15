1da11114b8c4fe91e8c134b3298d387350568cf9
Lukas Winkler <l.winkler23@me.com>
2014-09-15 17:25:59 +0200

    3d-Druck hinzugefügt

---
+++ 3ddruck/3ddruck.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir ~/3ddruck
+cd ~/3ddruck
+wget http://dl.slic3r.org/linux/slic3r-linux-x86-1-1-7-stable.tar.gz
+tar -xzf slic3r-linux-x86-1-1-7-stable.tar.gz
+rm slic3r-linux-x86-1-1-7-stable.tar.gz
+ln -s Slic3r/bin/slic3r Slic3r_starten
+mv ~/la-tools/3ddruck/Slic3r_config_bundle.ini einstellungen.ini
+mv ~/la-tools/3ddruck/tinkercad.desktop tinkercad.desktop
