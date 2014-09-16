eeffe93edb76cfb963f5d8a5961281daf1cb1b58
Lukas Winkler <l.winkler23@me.com>
2014-09-16 16:36:22 +0200

    3d-Druck: kopieren statt verschieben

--- 3ddruck/3ddruck.sh
+++ 3ddruck/3ddruck.sh
@@ -5,5 +5,5 @@ wget http://dl.slic3r.org/linux/slic3r-linux-x86-1-1-7-stable.tar.gz
 tar -xzf slic3r-linux-x86-1-1-7-stable.tar.gz
 rm slic3r-linux-x86-1-1-7-stable.tar.gz
 ln -s Slic3r/bin/slic3r Slic3r_starten
-mv ~/la-tools/3ddruck/Slic3r_config_bundle.ini einstellungen.ini
-mv ~/la-tools/3ddruck/*.desktop ./
+cp ~/la-tools/3ddruck/Slic3r_config_bundle.ini einstellungen.ini
+cp ~/la-tools/3ddruck/*.desktop ./
