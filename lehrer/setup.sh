c6cf75dda3d76eceecf2696bf3848242fc433dfd
Findus23 <l.winkler23@me.com>
2015-03-11 19:07:14 +0100

    Paketupdates

--- lehrer/setup.sh
+++ lehrer/setup.sh
@@ -18,6 +18,8 @@ sudo apt-get install pepperflashplugin-nonfree # Chromium
 
 if  [ "$1" == "update" ]
 then
+	sudo apt-get update # einige wichtige Pakete aktualisieren:
+	sudo apt-get install -y --only-upgrade chromium chromium-l10n iceweasel iceweasel-l10n-de geogebra5 vlc
 	sudo update-flashplugin-nonfree
 	sudo update-pepperflashplugin-nonfree
 	echo "test"
